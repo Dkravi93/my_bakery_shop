ce376963d1c202bb58f7692ae4862a054c7418ab
Deepak Kumar Ravi <dpakravi93@gmail.com>
2024-12-02 17:53:16 +0530

    Added the Home Page changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_bakery_shop/Pages/breakFast.dart';
 import 'package:my_bakery_shop/Pages/home.dart';
 
 void main() {
@@ -14,7 +15,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: Home()
+            initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/breakfast': (context) => BreakFast(),
+      },
     );
   }
 }
